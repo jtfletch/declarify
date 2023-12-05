37cc4b41e5fc09a7cd39e0239fd96e571a6388bd
jtfletch <jobetfletcher@gmail.com>
2023-12-05 20:38:35 +1100

    Added more yaml depth parsing
    Testing getting subheadings in yaml files

--- main.go
+++ main.go
@@ -8,32 +8,42 @@ import (
 )
 
 // Function to parse toml file. All test implementation
-func parseToml() interface{} {
+func parseYAML() interface{} {
 	vi := viper.New()
 	vi.SetConfigFile("test.yaml")
+	vi.SetConfigType("yaml")
 	vi.ReadInConfig()
-	return vi.Get("arch.terminals")
+	return vi.Get("arch")
 }
 
 func main() {
+	// Use the Get() method to retrieve the value of the "arch" key
+	arch := parseYAML()
 
-	// Use the Get() method to retrieve the value of the "terminals" key
-	var terminals = parseToml()
+	// Check if the value is a map
+	if archMap, ok := arch.(map[string]interface{}); ok {
+		// Iterate over the map and print each subheading
+		for key, value := range archMap {
+			fmt.Printf("Subheading: %s\n", key)
 
-	// Check if the value is a slice
-	if terminalsSlice, ok := terminals.([]interface{}); ok {
-		// Iterate over the slice and print each terminal
-		for _, terminal := range terminalsSlice {
-			fmt.Println(terminal)
+			// Check if the subheading value is a slice
+			if subheadingSlice, ok := value.([]interface{}); ok {
+				// Iterate over the slice and print each element
+				for _, element := range subheadingSlice {
+					fmt.Println(element)
+				}
+			} else {
+				fmt.Printf("Value under %s is not a slice\n", key)
+			}
 		}
 	} else {
-		fmt.Println("Terminals key not found or not a slice")
+		fmt.Println("Arch key not found or not a map")
 	}
 
-	// testing reading in from command line
+	// Testing reading in from the command line
 	if len(os.Args) > 1 {
 		// Print the value of the first command line argument
-		fmt.Println("word:", os.Args[1])
+		fmt.Println("Word:", os.Args[1])
 	} else {
 		fmt.Println("No command line argument provided")
 	}
